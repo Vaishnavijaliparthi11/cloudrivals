0422214808e20fbc3d714546a08b8f94cd8a75fa
VAISHNAVI JALIPARTHI <jaliparthivaishnavi.21.it@anits.edu.in>
2025-07-08 18:57:09 +0530

    first message

---
+++ app.java
@@ -0,0 +1,6 @@
+public class app {
+    public static void main(String[] args) {
+        String productName = "RR Technosoft";
+        System.out.println("Product Name: " + productName);
+    }
+}
